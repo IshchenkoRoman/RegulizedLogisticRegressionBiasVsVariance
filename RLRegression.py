295fca507b2adf1b9743f2e8bf331af5f3db933f
rishchen <rishchen@localhost.localdomain>
2018-02-13 23:01:57 +0000

    minimize

--- RLRegression.py
+++ RLRegression.py
@@ -30,10 +30,38 @@ class RLRegr():
 
 		# add bias parametr
 		self.X = np.c_[np.ones((self._Xshape[0], 1)), self.X]
+		self._Xshape = self.X.shape
 
 	def sigmoid(self, data):
 
 		return (1 / (np.exp(-data)))
+
+	# def gradient(self, theta, X, y, lambd):
+
+	# 	m = y.size
+
+	# 	h = X.dot(theta.reshape(-1, 1))
+
+	# 	grad = (1 / m) * (X.T.dot(h-y)) + (lambd/m)*np.r_[[[0]], theta[1:].reshape(-1, 1)]
+
+	# 	return (grad.flatten())
+
+	def gradient(self, theta, X, y, lambd):
+
+		m = len(y)
+		# Here was problem just add reshape
+		hypotesis = np.dot(theta, X.T).reshape(-1, 1)
+		diff = hypotesis - y
+		r = np.dot(diff.T, X)
+		grad = r / m
+
+		# print(theta.shape)
+		theta = theta.reshape(-1, 1)
+		# print(theta[1])
+		t = ((lambd / m) * theta[1:])
+		for i in range(1, grad.shape[1]):
+			grad[:,i] = grad[:,i] + t
+		return (grad.flatten())
 
 	def linearRegCostFunct(self, theta, X, y, lambd):
 
@@ -47,24 +75,23 @@ class RLRegr():
 		err = summ + l
 		return (err)
 
-	def gradient(self, theta, X, y, lambd):
+	# def linearRegCostFunct(self, theta, X, y, lambd):
 
-		m = len(y)
-		hypotesis = np.dot(theta, X.T).T
-		diff = hypotesis - y
-		r = np.dot(diff.T, X.T)
-		grad = r / m
+	# 	m = y.size
 
-		for i in range(1, grad.shape[1]):
-			grad[:,i] += ((lambd / m) * theta[:,1])
-		return (grad[0])		
+	# 	h = X.dot(theta)
 
-	def optimize(self, X, y, theta, lambd):
+	# 	cost = (1 / (2 * m)) * np.sum(np.square(h-y)) + (lambd / (2 * m)) * np.sum(np.square(theta[1:]))
+
+	# 	return (cost)		
+
+	def optimize(self, theta, X, y, lambd):
 		
-		print(X.shape)
+		# theta = np.array([[0], [0]])
+		print(theta.shape)
 		op = minimize(self.linearRegCostFunct, x0=theta, args=(X, y, lambd), method=None, jac=self.gradient, options={'maxiter':200})
 
-		return (op.x)
+		return (op)
 
 	def plotData(self, X, y, xlabel='x', ylabel='y'):
 
@@ -82,8 +109,8 @@ def main():
 	rlr = RLRegr(path_data)
 
 	# rlr.plotData(rlr.X[:,1], rlr.y, "Change in weather level (x)", "Wather flowing out of the dam (y)")
-	# theta = np.ones((1,2))
-	theta = np.zeros((rlr._yshape[0], 1)).reshape(1,12)
+	theta = np.array([[15],[15]])
+	# theta = np.zeros((rlr._yshape[0], 1)).reshape(1,12)
 	# print(rlr.linearRegCostFunct(theta, rlr.X, rlr.y, 1))
 	# print(rlr.gradient(theta, rlr.X, rlr.y, 1))
 	print(rlr.optimize(theta, rlr.X, rlr.y, 0))

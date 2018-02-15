26d564ccc8e1e4edb8653a537fe3f6910f65f975
rishchen <rishchen@localhost.localdomain>
2018-02-15 22:09:51 +0000

    Test error compute

--- RLRegression.py
+++ RLRegression.py
@@ -85,12 +85,12 @@ class RLRegr():
 
 		J = (1./(2*m)) * np.power((np.dot(X, theta) - y), 2).sum() + (float(lambd) / (2*m)) * np.power(theta[1:theta.shape[0]], 2).sum()
 
-		grad = (1./m) * np.dot(X.T, np.dot(X, theta) - y) + (float(lambd) / m)*theta
-
-		grad_no_regularization = (1./m) * np.dot(X.T, np.dot(X, theta) - y)
-		grad[0] = grad_no_regularization[0]
-
 		if return_grad == True:
+
+			grad = (1./m) * np.dot(X.T, np.dot(X, theta) - y) + (float(lambd) / m)*theta
+			grad_no_regularization = (1./m) * np.dot(X.T, np.dot(X, theta) - y)
+			grad[0] = grad_no_regularization[0]
+
 			return J, grad.flatten()
 		else:
 			return J
@@ -108,7 +108,7 @@ class RLRegr():
 		op = minimize(self.myLinearRegCostFunct, x0=theta, args=(X, y, lambd), method=None, jac=self.myGradient, options={'maxiter':200})
 		return (op)
 
-	def optimizeWithLBFGSB(self, theta, X, y, lambd):
+	def optimizeWithLBFGSB(self, X, y, lambd):
 		
 		theta = np.zeros((X.shape[1], 1))
 		op = minimize(self.linearRegCostFunct, x0=theta, args=(X, y, lambd, True), method='L-BFGS-B', jac=True, options={'disp':True, 'maxiter':200})
@@ -153,7 +153,7 @@ class RLRegr():
 		array_of_validate_error = []
 		theta_opt = np.zeros((X.shape[1], 1))
 		for i in range(0, m):
-			optimize = self.optimizeWithLBFGSB(theta_opt, X[:i+1], y[:i+1], lambd)
+			optimize = self.optimizeWithLBFGSB(X[:i+1], y[:i+1], lambd)
 			# print(optimize)
 			array_of_train_error.append(self.linearRegCostFunct(optimize.x, X[:i+1], y[:i+1], 0))
 			array_of_validate_error.append(self.linearRegCostFunct(optimize.x, validateX, validatey, 0))
@@ -198,7 +198,6 @@ class RLRegr():
 
 	def _plotFit(self, min_x, max_x, mu, sigma, theta, p, left=15, right=25):
 		
-		print(min_x)
 		x = np.array(np.arange(min_x - left, max_x + right, 0.05))
 
 		polynom = self.createPolynomX(p, x)
@@ -212,12 +211,12 @@ class RLRegr():
 	def plotPolyReg(self, polyX, y, mu, sigma, p, lambd=1):
 
 		init_theta = np.zeros((polyX.shape[1], 1))
-		theta = self.optimizeWithLBFGSB(init_theta, polyX, y, lambd)
+		theta = self.optimizeWithLBFGSB(polyX, y, lambd)
 
 		data = plt.plot(self.X[:,1], y, 'rx', markersize=7, mew=2)
 		pol = self._plotFit(min(self.X[:,1]), max(self.X[:,1]), mu, sigma, theta.x, p)
-		plt.xlim(-80, 60)
-		plt.ylim(-50, 300)
+		# plt.xlim(-80, 60)
+		# plt.ylim(min(self.X[:,1]) - min(self.X[:,1]) % 10, max(self.X[:,1]) + max(self.X[:,1]) % 10)
 		plt.xlabel("Change in water level (x)", fontsize=10)
 		plt.ylabel("Water flowing out of the dam (y)", fontsize=10)
 		plt.legend([data[0], pol[0]], ["Our data", "Polynomial curve of {0} degree".format(p)])
@@ -229,15 +228,66 @@ class RLRegr():
 
 		plt.close()
 		m = y.size
-		print(Xpoly.shape, y.shape)
-		print(Xval.shape, yval.shape)
-		plt.figure(figsize=(12,8))
+		# plt.figure(figsize=(12,8))
 
 		err_train, err_val =self.learningCurves(Xpoly, y, Xval, yval, lambd)
-
 		p1, p2 = plt.plot(range(1, m+1), err_train, range(1, m+1), err_val, linewidth=2)
 
+		plt.axis([0, 13, 0, 100])
+		legend = plt.legend((p1, p2), ("Traine", "Validation"), fontsize=10)
+		for label in legend.get_lines():
+			label.set_linewidth(3)
+
+		plt.title("Polynomial Regression Learning Curve (lambda = {:f})".format(lambd), fontsize=10)
+		plt.xlabel("Number of training examples", fontsize=10)
+		plt.ylabel("Error")
+
 		plt.show()
+
+		print("Polynomial Regression (lambda = {:f})\n\n".format(lambd))
+		print("# Training Examples\tTrain Error\tValidation Error\n")
+		for i in range(m):
+			print("  \t{:d}\t\t{:f}\t{:f}\n".format(i+1, float(err_train[i]), float(err_val[i])))
+
+	def _validationCurve(self, X, y, Xval, yval):
+
+		lambdas = np.array([0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
+		len_lambdas = lambdas.size
+
+		err_train = np.zeros((1, len_lambdas))[0]
+		err_val = np.zeros((1, len_lambdas))[0]
+		theta = 0
+		print(err_val)
+		# theta_array = np.zeros((1, len_lambdas))
+
+		for i in range(len_lambdas):
+
+			theta = self.optimizeWithLBFGSB(X, y, lambdas[i])
+			err_train[i] = self.linearRegCostFunct(theta.x, X, y, 0)
+			err_val[i] = self.linearRegCostFunct(theta.x, Xval, yval, 0)
+
+		return (lambdas, err_train, err_val)
+
+	def plotDifferentLambdasError(self, X, y, Xval, yval):
+
+		lambdas_vec, err_train, err_val = self._validationCurve(X, y, Xval, yval)
+
+		p1, p2 = plt.plot(lambdas_vec, err_train, lambdas_vec, err_val)
+		legend = plt.legend((p1, p2), ("Train", "Validation"), fontsize=10)
+		for label in legend.get_lines():
+			label.set_linewidth(3)
+
+		plt.xlabel(" Lambda ", fontsize=10)
+		plt.ylabel(" Error ")
+		# plt.axis([0, 10, 0, 20])
+
+		plt.show()
+
+		print("Lambda\t\tTrain Error\tValidation Error\n")
+		for i in range(len(lambdas_vec)):
+			print("  {:f}\t{:f}\t{:f}\n".format(lambdas_vec[i], err_train[i], err_val[i]))
+
+
 
 
 def main():
@@ -288,8 +338,55 @@ def main():
 	polynom_val = (polynom_val - mu) / sigma
 	polynom_val = np.c_[np.ones((polynom_val.shape[0], 1)), polynom_val]
 
+	# Here and below we can see how lambda is influence on accurancy of prediction
+
 	# rlr.plotPolyReg(polynom, rlr.y, mu, sigma, p, 0)
-	rlr.plotPolyLearningCurves(polynom, rlr.y, polynom_val, rlr.yval, 0)
+	# rlr.plotPolyLearningCurves(polynom, rlr.y, polynom_val, rlr.yval, 0)
+
+	# rlr.plotPolyReg(polynom, rlr.y, mu, sigma, p, 1)
+	# rlr.plotPolyLearningCurves(polynom, rlr.y, polynom_val, rlr.yval, 1)
+
+	# rlr.plotPolyReg(polynom, rlr.y, mu, sigma, p, 100)
+	# rlr.plotPolyLearningCurves(polynom, rlr.y, polynom_val, rlr.yval, 100)
+
+	# Plot different lambdas and print errors of lambdas, to understand which one is the best solution for our task
+
+	# rlr.plotDifferentLambdasError(polynom, rlr.y, polynom_val, rlr.yval)
+
+	"""
+
+	Lambda		Train Error	Validation Error
+
+	  0.000000	0.028890	53.919105
+
+	  0.001000	0.107976	9.349919
+
+	  0.003000	0.166731	15.920055
+
+	  0.010000	0.217958	17.149315
+
+	  0.030000	0.275149	13.216183
+
+	  0.100000	0.438656	7.926826
+
+	  0.300000	0.868209	4.760732
+
+	  1.000000	1.958697	4.263453
+
+	  3.000000	4.525101	3.832184
+
+	  10.000000	14.825780	8.889697
+
+
+	We see that our lambda has value nearly 3.832184
+	"""
+
+	lambd = 3
+	theta = rlr.optimizeWithLBFGSB(polynom, rlr.y, lambd)
+
+	err = rlr.linearRegCostFunct(theta.x, polynom_test, rlr.ytest, 0)
+	print(err)
+
 
 
 if __name__ == '__main__':
